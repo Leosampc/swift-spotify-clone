280de8a2ba661736fc673063f7adeb4e39e398bf
leosampc <leonardosamp11@gmail.com>
2022-06-03 15:08:46 -0300

    initial project structure

---
+++ Spotify Clone/Models/AudioTrack.swift
@@ -0,0 +1,8 @@
+//
+//  AudioTrack.swift
+//  Spotify Clone
+//
+//  Created by Leonardo Cruz on 03/06/22.
+//
+
+import Foundation
---
+++ Spotify Clone/Models/UserProfile.swift
@@ -0,0 +1,8 @@
+//
+//  UserProfile.swift
+//  Spotify Clone
+//
+//  Created by Leonardo Cruz on 03/06/22.
+//
+
+import Foundation
