52dcd1569c824fc554e4110d9df5a39cfa7cad98
= <=>
2022-06-03 14:52:24 -0300

    initial commit

---
+++ Spotify Clone/Controllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Spotify Clone
+//
+//  Created by Leonardo Cruz on 03/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // testing view code config
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
