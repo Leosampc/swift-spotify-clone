b5ff50a4fabc9e8b37f05743c42a0906a7bb7284
leosampc <leonardosamp11@gmail.com>
2022-06-04 15:15:25 -0300

    caching auth parameters and navigate to TabBarViewController if login is successful

--- Spotify Clone/Controllers/Other/WelcomeViewController.swift
+++ Spotify Clone/Controllers/Other/WelcomeViewController.swift
@@ -39,7 +39,7 @@ class WelcomeViewController: UIViewController {
     @objc func didTapSignIn() {
         let authVC = AuthViewController()
         authVC.completionHandler = { [weak self] success in
-            DispatchQueue.main.sync {
+            DispatchQueue.main.async {
                 self?.handleSignIn(success)
             }
         }
@@ -48,6 +48,16 @@ class WelcomeViewController: UIViewController {
     }
 
     private func handleSignIn(_ success: Bool) {
+        guard success else {
+            let errorAlert = UIAlertController(title: "Ops!", message: "Algo deu errado com o seu login", preferredStyle: .alert)
+            let dismissAction = UIAlertAction(title: "Fechar", style: .cancel)
+            errorAlert.addAction(dismissAction)
+            present(errorAlert, animated: true)
+            return
+        }
         
+        let mainAppTabBarVC = TabBarViewController()
+        mainAppTabBarVC.modalPresentationStyle = .fullScreen
+        present(mainAppTabBarVC, animated: true)
     }
 }
--- Spotify Clone/Managers/AuthManager.swift
+++ Spotify Clone/Managers/AuthManager.swift
@@ -27,23 +27,25 @@ final class AuthManager {
     }
     
     var isSignedIn: Bool {
-        return false
+        return accessToken != nil
     }
     
     private var accessToken: String? {
-        return nil
+        return UserDefaults.standard.string(forKey: "access_token")
     }
     
     private var refreshToken: String? {
-        return nil
+        return UserDefaults.standard.string(forKey: "refresh_token")
     }
     
     private var tokenExpirationDate: Date? {
-        return nil
+        return UserDefaults.standard.object(forKey: "expirationDate") as? Date
     }
     
     private var shouldRefreshToken: Bool {
-        return false
+        guard let expirationDate = tokenExpirationDate else { return false }
+        let fiveMinutes: TimeInterval = 300
+        return Date().addingTimeInterval(fiveMinutes) >= expirationDate
     }
     
     public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
@@ -70,15 +72,16 @@ final class AuthManager {
         request.setValue("Basic \(base64Token)", forHTTPHeaderField: "Authorization")
         request.httpBody = components.query?.data(using: .utf8)
         
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 completion(false)
                 return
             }
             
             do {
-                let json = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
-                print("SUCCESS: \(json)")
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                self?.cacheToken(result: result)
+                completion(true)
             } catch {
                 print(error.localizedDescription)
                 completion(false)
@@ -91,7 +94,9 @@ final class AuthManager {
         
     }
     
-    private func cacheToken() {
-        
+    private func cacheToken(result: AuthResponse) {
+        UserDefaults.standard.set(result.access_token, forKey: "access_token")
+        UserDefaults.standard.set(result.refresh_token, forKey: "refresh_token")
+        UserDefaults.standard.set(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expirationDate")
     }
 }
